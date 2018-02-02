8011cd2dcfac55ecec8dd1338fad262c31459805
Heather <hmaskell@sfu.ca>
2018-02-02 12:52:49 -0800

    Implement functions
    Implemented max, min, sum, avg and count to work with dataset

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
-var dataset;
 d3.json("http://www.sfu.ca/~hmaskell/iat335/top-rated-movies-01.json",function(error,data){
 	if(error){
 		console.log(error);	
 	}
 	else{
 		console.log(data);
-		dataset= data;
 		d3.select("body").selectAll("p")
-			.data(dataset)
+			.data(data)
 			.enter()
 			.append("p")
 			.text(function(d){
-				if (d.originalTitle != ""){
+				if (d.originalTitle == "The Shawshank Redemption"){
+					return d.originalTitle+" "+d.imdbRating;
+				}
+				else if (d.originalTitle != ""){
 					return d.originalTitle+" "+d.imdbRating;
 				}
 				else{
@@ -19,68 +20,69 @@ d3.json("http://www.sfu.ca/~hmaskell/iat335/top-rated-movies-01.json",function(e
 				}
 			}
 		);
+		callMethods(data);
 	}
+});
 
-	var arrayLength = data.length;
-	var max = maximum(data);
-	var min = minimum(data);
-	var sum = sum(data);
-	var avg = avg(sum, data.length);
-	var count = count(data, "2");
+function callMethods(dataset){
+	var arrayLength = dataset.length;
+	var max = findmaximum(dataset);
+	var min = findminimum(dataset);
+	var sum = findsum(dataset);
+	var avg = findavg(sum, dataset.length);
+	var count = findcount(dataset, "8.3");
 
-	console.log("The maximum rating is "+max);
+	console.log("The final maximum rating is "+max);
 	console.log("The minimum rating is "+min);
 	console.log("The sum of all IMDB ratings is "+sum);
-	console.log("The count of '8.8' ratings is "+count);
+	console.log("The count of '8.3' ratings is "+count);
 	console.log("The average rating is "+avg);
+}
 
-
-	function maximum(data){
-		var max = -1;
-		for (var i=0; i<data.length; i++){
-			if (data[i] >= max){
-				max = data[i];
-				//console.log("The new maximum value is "+max);
-			}
+function findmaximum(data){
+	var max = -1;
+	for (var i=0; i<data.length; i++){
+		if (data[i].imdbRating >= max){
+			max = data[i].imdbRating;
+			//console.log("The new maximum value is "+max);
 		}
-		return max;
 	}
+	return max;
+}
 
-	function minimum(data){
-		var min = 10000;
-		for (var i=0; i<data.length; i++){
-			if (data[i] <= min){
-				min = data[i];
-				//console.log("The new minimum value is "+min);
-			}
+function findminimum(data){
+	var min = 10000;
+	for (var i=0; i<data.length; i++){
+		if (data[i].imdbRating <= min){
+			min = data[i].imdbRating;
+			//console.log("The new minimum value is "+min);
 		}
-		return min;
 	}
+	return min;
+}
 
 
-	function sum(data){
-		var sum = 0;
-		for (var i=0; i<data.length; i++){
-			sum += data[i];
+function findsum(data){
+	var sum = 0;
+	for (var i=0; i<data.length; i++){
+		sum += data[i].imdbRating;
+	}
+	return sum;
+}
+
+
+
+function findavg(sum, length){
+	var avg = sum/length;
+	return avg;
+}
+
+function findcount(data, value){
+	var count = 0;
+	for (var i=0; i<data.length; i++){
+		if (data[i].imdbRating == value){
+			count++;
 		}
-		return sum;
 	}
-
-
-
-	function avg(sum, length){
-		var avg = sum/length;
-		return avg;
-	}
-
-	function count(data, value){
-		var count = 0;
-		for (var i=0; i<data.length; i++){
-			if (data[i] == value){
-				count++;
-			}
-		}
-		return count;
-	} 
-
-});
+	return count;
+} 
