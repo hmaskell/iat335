434dc5e2f790cade9a388e4cc0361d40cb4c7bc9
Heather <hmaskell@sfu.ca>
2018-02-02 11:51:24 -0800

    Set up
    Set up functions to work in the scope of the data, changed index page d3 body stuff to show the Original title if it has one, and otherwise show the title.

--- js/scripts.js
+++ js/scripts.js
@@ -1,108 +1,86 @@
-//var json = readFile("file://../data/top-rated-movies-01.json");
-var data=[0,3,8,2,60,1,2,2];
-var arrayLength = data.length;
-
-/*var arrayLength = data.length;*/
-
-var max = maximum(data);
-var min = minimum(data);
-var sum = sum(data);
-var avg = avg(sum, arrayLength);
-var count = count(data, "2");
-
-
 var dataset;
 d3.json("http://www.sfu.ca/~hmaskell/iat335/top-rated-movies-01.json",function(error,data){
 	if(error){
 		console.log(error);	
-	}else{
+	}
+	else{
 		console.log(data);
 		dataset= data;
-	/*	d3.select("body").selectAll("p")
+		d3.select("body").selectAll("p")
 			.data(dataset)
 			.enter()
 			.append("p")
-			.text(function(d){return d.title+"  "+d.imdbRating;});*/
+			.text(function(d){
+				if (d.originalTitle != ""){
+					return d.originalTitle+" "+d.imdbRating;
+				}
+				else{
+					return d.title+" "+d.imdbRating;
+				}
+			}
+		);
 	}
+
+	var arrayLength = data.length;
+	var max = maximum(data);
+	var min = minimum(data);
+	var sum = sum(data);
+	var avg = avg(sum, data.length);
+	var count = count(data, "2");
+
+	console.log("The maximum rating is "+max);
+	console.log("The minimum rating is "+min);
+	console.log("The sum of all IMDB ratings is "+sum);
+	console.log("The count of '8.8' ratings is "+count);
+	console.log("The average rating is "+avg);
+
+
+	function maximum(data){
+		var max = -1;
+		for (var i=0; i<data.length; i++){
+			if (data[i] >= max){
+				max = data[i];
+				//console.log("The new maximum value is "+max);
+			}
+		}
+		return max;
+	}
+
+	function minimum(data){
+		var min = 10000;
+		for (var i=0; i<data.length; i++){
+			if (data[i] <= min){
+				min = data[i];
+				//console.log("The new minimum value is "+min);
+			}
+		}
+		return min;
+	}
+
+
+	function sum(data){
+		var sum = 0;
+		for (var i=0; i<data.length; i++){
+			sum += data[i];
+		}
+		return sum;
+	}
+
+
+
+	function avg(sum, length){
+		var avg = sum/length;
+		return avg;
+	}
+
+	function count(data, value){
+		var count = 0;
+		for (var i=0; i<data.length; i++){
+			if (data[i] == value){
+				count++;
+			}
+		}
+		return count;
+	} 
+
 });
-
-
-console.log("The final maximum value is "+max);
-console.log("The minimum value is "+min);
-console.log("The sum of all data values is "+sum);
-console.log("The count of '2' is "+count);
-console.log("The average value is "+avg);
-
-
-d3.json("http://www.sfu.ca/~hmaskell/iat335/top-rated-movies-01.json", function(json){
-	console.log(json);
-});
-
-
-
-//reference: https://stackoverflow.com/questions/14446447/how-to-read-a-local-text-file
-/*function readFile(filename){
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", filename, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                alert(allText);
-            }
-        }
-    }
-    rawFile.send(null);
-}
-*/
-
-function maximum(data){
-	var max = -1;
-	for (var i=0; i<arrayLength; i++){
-		if (data[i] >= max){
-			max = data[i];
-			//console.log("The new maximum value is "+max);
-		}
-	}
-	return max;
-}
-
-function minimum(data){
-	var min = 10000;
-	for (var i=0; i<arrayLength; i++){
-		if (data[i] <= min){
-			min = data[i];
-			//console.log("The new minimum value is "+min);
-		}
-	}
-	return min;
-}
-
-
-function sum(data){
-	var sum = 0;
-	for (var i=0; i<arrayLength; i++){
-		sum += data[i];
-	}
-	return sum;
-}
-
-
-
-function avg(sum, length){
-	var avg = sum/length;
-	return avg;
-}
-
-function count(data, value){
-	var count = 0;
-	for (var i=0; i<arrayLength; i++){
-		if (data[i] == value){
-			count++;
-		}
-	}
-	return count;
-} 
