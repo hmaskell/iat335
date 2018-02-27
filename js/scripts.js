ec71d41eee6b1384873b6a0834f6d9c57b67cd18
Heather <hmaskell@sfu.ca>
2018-02-26 20:22:00 -0800

    Added Date Sanitization, ListAll Funciton, and ListWhich function
    Date Sanitization takes in many different date formats, and converts them to a 4 digit year string. ListAll function lists all the unique values for a given dimension (returns a list of genres, or list of production companies). ListWhich function returns all matches for a given value (ie if you want to know which movies had a budget of $300,000,000).

--- js/scripts.js
+++ js/scripts.js
@@ -23,24 +23,32 @@ d3.json("http://www.sfu.ca/~hmaskell/iat335/movies_IMDB.json",function(error,dat
 
 function callMethods(dataset){
 	var arrayLength = dataset.length;
-	var max = findmaximum(dataset, "Production_Budget");
-	var min = findminimum(dataset, "IMDB_Rating");
+	var max = findmaximum(dataset, "Release_Date");
+	var min = findminimum(dataset, "Release_Date");
 	var sum = findsum(dataset, "Worldwide_Gross");
-	var avg = findavg(findsum(dataset, "IMDB_Rating"), dataset.length);
+	var avg = findavg(findsum(dataset, "Production_Budget"), dataset.length);
 	var count = findcount(dataset, "MPAA_Rating", "R");
+	var list = listAll(dataset, "Major_Genre");
+	var whichList = findWhich(dataset, "Production_Budget", "218");
 
-	console.log("The maximum Production Budget is $"+max);
-	console.log("The minimum IMDB rating is "+min);
-	console.log("The sum of Worldwide Gross Income for all movies in the dataset is $"+sum);
-	console.log("The count of all R rated movies in the dataset is "+count);
-	console.log("The average IMDB rating is "+avg);
+	console.log("The maximum value is is "+max);
+	//console.log("The minimum value is "+min);
+	//console.log("The sum of Worldwide Gross Income for all movies in the dataset is $"+sum);
+	//console.log("The count of all R rated movies in the dataset is "+count);
+	//console.log("The average is "+avg);
+	//console.log("The list is as follows: "+list);
+	//console.log("The list of items that match the parameters includes: "+whichList);
 }
 
 function findmaximum(data, dimension){
 	var max = -1;
 	for (var i=0; i<data.length; i++){
-		if ((data[i][dimension] != null) && (data[i][dimension] >= max)){
-			max = data[i][dimension];
+		var item = data[i][dimension];
+		if (dimension == "Release_Date"){
+			item = convertDate(item);
+		}
+		if ((item != null) && (item != "") && (item >= max)){
+			max = item;
 			//console.log("The new maximum value is "+max);
 		}
 	}
@@ -48,13 +56,21 @@ function findmaximum(data, dimension){
 }
 
 function findminimum(data, dimension){
-	var min = 10000;
+	var min = 1000000000;
 	for (var i=0; i<data.length; i++){
-		if ((data[i][dimension] != null) && (data[i][dimension] < min)){
-			min = data[i][dimension];
-			console.log("The new minimum value is "+min);
+		var item = data[i][dimension];
+		//console.log("item = "+item);
+		if (dimension == "Release_Date"){
+			item = convertDate(item);
+			//console.log("converted item = "+item);
 		}
+		if ((item != null) && (item != "") && (item < min)){
+			min = item;
+			//console.log("The new minimum value is "+min);
+		}
+		//console.log("the minimum value within the for loop: "+min);
 	}
+	//console.log("the final minimum value within the funciton: "+min);
 	return min;
 }
 
@@ -62,8 +78,9 @@ function findminimum(data, dimension){
 function findsum(data, dimension){
 	var sum = 0;
 	for (var i=0; i<data.length; i++){
-		if (data[i][dimension] != null){
-			sum += data[i][dimension];
+		var item = data[i][dimension];
+		if (item != null){
+			sum += item;
 		}
 	}
 	return sum;
@@ -84,9 +101,80 @@ function findavg(sum, length){
 function findcount(data, dimension, value){
 	var count = 0;
 	for (var i=0; i<data.length; i++){
-		if (data[i][dimension] == value){
+		var item = data[i][dimension];
+		if (dimension == "Release_Date"){
+			item = convertDate(item);
+		}
+		if (item == value){
 			count++;
 		}
 	}
 	return count;
 } 
+
+function listAll(data, dimension){
+	var list = ["null", ""];
+	var already_exists = new Boolean(false);
+	for (var i=0; i<data.length; i++){
+		var item = data[i][dimension];
+		if (dimension == "Release_Date"){
+			item = convertDate(item);
+		}
+		already_exists = false;
+		for (var j=0; j<list.length; j++){
+			//console.log("data[i]dimension = "+data[i][dimension]);
+			//console.log("list[j] = "+list[j]);
+			if (item == list[j]){
+				already_exists = true;
+				//console.log("already exists");
+			}
+		} 
+		if (already_exists == false){
+			list.push(item);
+		}
+	}
+	//console.log("the final list is: "+list);
+	return list;
+}
+
+function findWhich(data, dimension, value){
+	var list = [""];
+	for (var i=0; i<data.length; i++){
+		var item = data[i][dimension];
+		if (dimension == "Release_Date"){
+			item = convertDate(item);
+		}
+		if (item == value){
+			list.push(data[i]["Title"]);
+		}
+	}
+	return list;
+
+}
+
+function convertDate(date){
+	var fourDigits = /\d{4}/;  //using regex to check if there are 4 consecutive digits
+	var fourDigitYear ="";
+	if ((date!=null) && (date[date.length - 3] == "-")){
+		//console.log("MM-DD-YY: "+date);
+		var twoDigitYear = date.slice(-2);
+		if (twoDigitYear>17){
+			fourDigitYear = "19"+twoDigitYear;
+		}
+		else{
+			fourDigitYear = "20"+twoDigitYear;
+		}
+	}
+	// if there are four consecutive digits in the date string
+	else if (fourDigits.test(date)){ 
+		if (date[0].match(/[a-z]/i)){ //checks to see if the first character in the string is a letter
+			fourDigitYear = date.split(" ").pop() //isolates everything in string after the space
+			//console.log("Month, YYYY: "+date);
+		}
+		else{
+			fourDigitYear = date.slice[0,3];
+			//console.log("YYYY, Month: "+date)
+		}
+	}
+	return fourDigitYear;
+}
